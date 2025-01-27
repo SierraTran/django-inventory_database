d7af329bd4d89f2b71014b20adebab90e0716589
Sierra Tran <69904865+SierraTran@users.noreply.github.com>
2025-01-27 16:01:23 -0500

    Added part_number field

--- inventory/models.py
+++ inventory/models.py
@@ -9,11 +9,28 @@ class Item(models.Model):
     manufacturer = models.CharField(max_length=50)
     model = models.CharField(max_length=100)
     part_or_unit = models.CharField(blank=False, choices=ItemType, max_length=10)
+    part_number = models.CharField(blank=True, max_length=100)
     description = models.TextField()
     location = models.CharField(max_length=50)
     quantity = models.IntegerField(default=0, validators=[MinValueValidator(0)])
-    price = models.DecimalField(max_digits=16, decimal_places=2, default=0.01, validators=[MinValueValidator(0.00)])
+    price = models.DecimalField(
+        max_digits=16,
+        decimal_places=2,
+        default=0.01,
+        validators=[MinValueValidator(0.00)],
+    )
 
     def __str__(self):
+        """
+        The string representation of the `Item` object
+
+        Parameters:
+            self (Item): The current `Item` object
+
+        Returns:
+            str: The `model` and `manufacturer` of the object separated by a comma.
+
+        Example:
+            "87 Case, HP"
+        """
         return self.model + ", " + self.manufacturer
-    
