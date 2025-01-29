5afb046d7a7ca6871bed823540ec82688b61aab6
Sierra Tran <69904865+SierraTran@users.noreply.github.com>
2025-01-29 12:03:31 -0500

    Docstrings and item details attempt
    I made some docstrings with the help of GitHub Copilot. I'm still trying to get item details to show on the page.

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.views import generic
@@ -9,10 +9,25 @@ from .models import Item
 # Create your views here.
 # @login_required
 class ItemView(generic.ListView):
+    """
+    View to list all items.
+    
+    Attributes:
+        template_name (str): The name of the template to use for rendering the view.
+        context_object_name (str): The name of the context variable to use for the list of items.
+    Methods:
+        get_queryset(): Retrieves the list of items to be displayed.
+    """
     template_name = "items.html"
     context_object_name = "items_list"
 
     def get_queryset(self):
+        """
+        Retrieves the list of items to be displayed.
+
+        Returns:            
+            QuerySet: A queryset containing all items.
+        """
         return Item.objects.all()
 
 
@@ -20,8 +35,12 @@ class ItemView(generic.ListView):
 class ItemDetailsView(generic.ListView):
     model = Item
     template_name = "item-details.html"
+    context_object_name = "item"
+    
     
 
-    def get_queryset(self):
-        
-        return Item.objects.all()
+
+def getItemDetails(request, pk):
+    item = get_object_or_404(Item, pk=pk).__dict__
+
+    return render(request, "item-details.html", item)
