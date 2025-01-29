047d459a5f4c87e4865933cc6e86d8eb3766fb4a
Sierra Tran <69904865+SierraTran@users.noreply.github.com>
2025-01-29 15:39:38 -0500

    Update tests.py

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
+from .models import Item
+
 # Create your tests here.
+class ItemModelTests(TestCase):
+    def create_item():
+        """
+        Create an item.
+        """
