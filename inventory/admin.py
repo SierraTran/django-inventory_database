ea321d62ab578dd7c1c4779ac64a7768611840a1
Sierra Tran <69904865+SierraTran@users.noreply.github.com>
2025-01-30 15:27:51 -0500

    Formatting and additional field ordering

--- inventory/admin.py
+++ inventory/admin.py
@@ -3,15 +3,19 @@ from .models import Item
 
 
 class ItemAdmin(admin.ModelAdmin):
-    # This will list the items on the admin site 
+    # This will list the items on the admin site
     # using the fields `manufacturer`, `model`, and `part_number`
-    list_display = ["manufacturer", "model", "part_number"]
+    list_display = [
+        "manufacturer",
+        "model",
+        "part_number",
+    ]
 
     # Items can be filtered for being a part or unit
     list_filter = ["part_or_unit"]
-    
+
     # Items are alphabetically ordered by manufacturer and then model
-    ordering = ["manufacturer", "model"]
+    ordering = ["manufacturer", "model", "part_number"]
 
 
 # Register your models here.
