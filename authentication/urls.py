de5b42d38023905f7fe1a568208b51187a417426
Sierra Tran <69904865+SierraTran@users.noreply.github.com>
2025-01-30 15:29:15 -0500

    Uncommented home url path

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "authentication"
 
 urlpatterns = [
-    #path('', views.home, name='home'),
+    path('', views.home, name='home'),
     #path('login/', views.login_page, name='login'),
     #path('logout/', views.logout_view, name="logout"),
 ]
