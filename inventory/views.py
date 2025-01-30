9cb1701ee759269ff790ea46f29601c813500901
Sierra Tran <69904865+SierraTran@users.noreply.github.com>
2025-01-30 09:44:15 -0500

    Displayed item details on the page with help from GitHub Copilot

--- inventory/views.py
+++ inventory/views.py
@@ -12,13 +12,15 @@ from .models import Item
 class ItemView(ListView):
     """
     View to list all items.
-    
+
     Attributes:
         template_name (str): The name of the template to use for rendering the view.
         context_object_name (str): The name of the context variable to use for the list of items.
+
     Methods:
         get_queryset(): Retrieves the list of items to be displayed.
     """
+
     model = Item
     template_name = "items.html"
     context_object_name = "items_list"
@@ -26,25 +28,25 @@ class ItemView(ListView):
     def get_queryset(self):
         """
         Retrieves the list of items to be displayed.
+        Items are alphanumerically ordered (ascending) by `manufacturer` first and model second.
 
-        Returns:            
+        Returns:
             QuerySet: A queryset containing all items.
         """
-        return Item.objects.all()
-    
-    
-    
-    
+        return Item.objects.all().order_by("model").order_by("manufacturer")
 
 
 # @login_required
 class ItemDetailsView(TemplateView):
     model = Item
     template_name = "item-details.html"
-    
+    context_object_name = "item"
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
 
 
 def getItemDetails(request, pk):
-    item = get_object_or_404(Item, pk=pk).__dict__
-
-    return render(request, "item-details.html", item)
+    item = get_object_or_404(Item, pk=pk)
+    context = {"item": item}
+    return render(request, "item-details.html", context)
