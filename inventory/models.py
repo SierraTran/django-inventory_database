411e92a4479a4cea6da1f412b39cc0989b551d0b
Sierra Tran <69904865+SierraTran@users.noreply.github.com>
2025-01-31 11:39:52 -0500

    Item Model adjustments

--- inventory/models.py
+++ inventory/models.py
@@ -6,17 +6,17 @@ from django.core.validators import MinValueValidator
 class Item(models.Model):
     ItemType = models.TextChoices("ItemType", "Part Unit")
 
-    manufacturer = models.CharField(max_length=50)
-    model = models.CharField(max_length=100)
+    manufacturer = models.CharField(default="N/A", max_length=50)
+    model = models.CharField(default="N/A", max_length=100)
     part_or_unit = models.CharField(blank=False, choices=ItemType, max_length=10)
     part_number = models.CharField(blank=True, max_length=100)
     description = models.TextField()
-    location = models.CharField(max_length=50)
+    location = models.CharField(default="N/A", max_length=50)
     quantity = models.IntegerField(default=0, validators=[MinValueValidator(0)])
     price = models.DecimalField(
+        default=0.01,
         max_digits=16,
         decimal_places=2,
-        default=0.01,
         validators=[MinValueValidator(0.00)],
     )
 
