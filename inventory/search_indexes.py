3628443cc5f8faf4f155b23d8b7b67929ac88c5a
Sierra Tran <69904865+SierraTran@users.noreply.github.com>
2025-02-05 11:31:29 -0500

    Implemented search functionality with Whoosh

---
+++ inventory/search_indexes.py
@@ -0,0 +1,19 @@
+from haystack import indexes
+from .models import Item
+
+class ItemIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    manufacturer = indexes.CharField(model_attr='manufacturer')
+    model = indexes.CharField(model_attr='model')
+    part_or_unit = indexes.CharField(model_attr='part_or_unit')
+    part_number = indexes.CharField(model_attr='part_number')
+    description = indexes.CharField(model_attr='description')
+    location = indexes.CharField(model_attr='location')
+    quantity = indexes.IntegerField(model_attr='quantity')
+    price = indexes.DecimalField(model_attr='price')
+
+    def get_model(self):
+        return Item
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
