159f145dad6dd6c31e3a2bcb5849bddca484d174
Sierra Tran <69904865+SierraTran@users.noreply.github.com>
2025-02-05 12:48:11 -0500

    Adjust url paths for inventory app

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,12 +6,12 @@ app_name = "inventory"
 
 urlpatterns = [
     # example: /inventory_database/items
-    path("items/", views.ItemView.as_view(), name="items"),
+    path("", views.ItemView.as_view(), name="items"),
     # example: /inventory_database/items/9
     #path("items/<int:pk>/", views.getItemDetails, name="item_details"), [OLD]
-    path("items/<int:pk>/", views.ItemDetailView.as_view(), name="item_detail"),
+    path("<int:pk>/", views.ItemDetailView.as_view(), name="item_detail"),
     #path("items/<int:pk>/update", views.updateItemAsSuperuser, name="update-item-superuser"), [OLD]
-    path("items/<int:pk>/update", views.ItemUpdateView.as_view(), name="item_update_form"),
-    path("items/<int:pk>/quantity_update", views.ItemQuantityUpdateView.as_view(), name="item_quantity_update_form"),
-    path('items/search/', views.search_items, name='search_items'),
+    path("<int:pk>/update", views.ItemUpdateView.as_view(), name="item_update_form"),
+    path("<int:pk>/quantity_update", views.ItemQuantityUpdateView.as_view(), name="item_quantity_update_form"),
+    path('search/', views.search_items, name='search_items'),
 ]
