296f3c00f1e868aaff402859b216a9199c7a522f
Sierra Tran <69904865+SierraTran@users.noreply.github.com>
2025-02-07 13:54:27 -0500

    Deleted old url paths

--- inventory/urls.py
+++ inventory/urls.py
@@ -8,10 +8,8 @@ urlpatterns = [
     # example: /inventory_database/items
     path("", views.ItemView.as_view(), name="items"),
     # example: /inventory_database/items/9
-    #path("items/<int:pk>/", views.getItemDetails, name="item_details"), [OLD]
     path("<int:pk>/", views.ItemDetailView.as_view(), name="item_detail"),
     path("new_item_form/", views.ItemCreateView.as_view(), name="item_create_form"),
-    #path("items/<int:pk>/update", views.updateItemAsSuperuser, name="update-item-superuser"), [OLD]
     path("<int:pk>/update", views.ItemUpdateView.as_view(), name="item_update_form"),
     path("<int:pk>/quantity_update", views.ItemQuantityUpdateView.as_view(), name="item_quantity_update_form"),
     path('search/', views.SearchItemsView.as_view(), name='search_items'),
