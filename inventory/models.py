13661771a2a307b7f75f353de636d7fa678f718b
Sierra Tran <69904865+SierraTran@users.noreply.github.com>
2025-02-07 13:55:51 -0500

    Added Enum for Item model

--- inventory/models.py
+++ inventory/models.py
@@ -5,30 +5,42 @@ from django.urls import reverse
 
 # Create your models here.
 class Item(models.Model):
-    ItemType = models.TextChoices("ItemType", "Part Unit")
+    PART = "Part"
+    UNIT = "Unit"
+
+    ITEM_TYPE_CHOICES = {
+        PART: "Part",
+        UNIT: "Unit",
+    }
 
     manufacturer = models.CharField(default="N/A", max_length=50)
     model = models.CharField(default="N/A", max_length=100)
-    part_or_unit = models.CharField(blank=False, choices=ItemType, max_length=10)
+    part_or_unit = models.CharField(
+        blank=False,
+        choices=ITEM_TYPE_CHOICES,
+        default=PART,
+        max_length=5,
+    )
     part_number = models.CharField(blank=True, max_length=100)
     description = models.TextField()
     location = models.CharField(default="N/A", max_length=50)
     quantity = models.IntegerField(default=0, validators=[MinValueValidator(0)])
-    quantity_min_stock = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    quantity_min_stock = models.IntegerField(
+        default=0, validators=[MinValueValidator(0)]
+    )
     price = models.DecimalField(
         default=0.01,
         max_digits=16,
         decimal_places=2,
         validators=[MinValueValidator(0.00)],
     )
-    
+
     @property
     def low_stock(self):
         return self.quantity <= self.quantity_min_stock
-    
+
     def get_absolute_url(self):
         return reverse("inventory:item_detail", kwargs={"pk": self.pk})
-    
 
     def __str__(self):
         """
