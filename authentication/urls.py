09207335715c66d2a687cafd81205c631c7f8f55
Sierra Tran <69904865+SierraTran@users.noreply.github.com>
2025-02-07 12:43:36 -0500

    Add package initialization files and update item templates for improved navigation and styling

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,6 +5,4 @@ app_name = "authentication"
 
 urlpatterns = [
     path('', views.home, name='home'),
-    #path('login/', views.login_page, name='login'),
-    #path('logout/', views.logout_view, name="logout"),
 ]
--- inventory/tests.py
+++ inventory/tests.py
@@ -2,9 +2,65 @@ from django.test import TestCase
 
 from .models import Item
 
+
 # Create your tests here.
 class ItemModelTests(TestCase):
-    def create_item():
+    def test_create_item_unit(self):
         """
-        Create an item.
+        Test that an item (unit) can be created.
         """
+        manufacturer = "Test MFG"
+        model = "Test Model"
+        part_or_unit = Item.UNIT
+
+        item = Item.objects.create(
+            manufacturer=manufacturer, model=model, part_or_unit=part_or_unit
+        )
+        self.assertTrue(isinstance(item, Item))
+        self.assertEqual(item.__str__(), "Test MFG, Test Model")
+
+    def test_create_item_part_blankpartnumber(self):
+        """
+        Test that an item (part) can be created.
+        """
+        manufacturer = "Test MFG2"
+        model = "Test Model2"
+        part_or_unit = Item.PART
+
+        item = Item.objects.create(
+            manufacturer=manufacturer, model=model, part_or_unit=part_or_unit
+        )
+        self.assertTrue(isinstance(item, Item))
+        self.assertEqual(item.__str__(), "Test MFG2, Test Model2 ")
+
+    def test_create_item_part_nonblankpartnumber(self):
+        """
+        Test that an item (part) can be created.
+        """
+        manufacturer = "Test MFG2"
+        model = "Test Model2"
+        part_or_unit = Item.PART
+        part_number = "Test Part Number"
+
+        item = Item.objects.create(
+            manufacturer=manufacturer,
+            model=model,
+            part_or_unit=part_or_unit,
+            part_number=part_number,
+        )
+        self.assertTrue(isinstance(item, Item))
+        self.assertEqual(item.__str__(), "Test MFG2, Test Model2 Test Part Number")
+
+    def test_item_quantity(self):
+        """
+        Test that the item quantity is correct.
+        """
+        item = Item.objects.create(quantity=20)
+        self.assertEqual(item.quantity, 20)
+
+    def test_item_price(self):
+        """
+        Test that the item price is correct.
+        """
+        item = Item.objects.create(price=200.0)
+        self.assertEqual(item.price, 200.0)
