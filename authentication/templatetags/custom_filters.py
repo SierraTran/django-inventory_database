581a07e6e50b88d6ec6c04842619f255fd86a471
Sierra Tran <69904865+SierraTran@users.noreply.github.com>
2025-03-04 13:21:32 -0500

    Item request functionality and updating users

--- authentication/templatetags/custom_filters.py
+++ authentication/templatetags/custom_filters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter(name="clean_id_for_label")
 def clean_id_for_label(value):
     return value.replace("id_", "")
+
+@register.filter(name="attr")
+def attr(field, attr):
+    return field.as_widget(attrs={attr: attr})
--- inventory/urls.py
+++ inventory/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path("items/<int:pk>/history", views.ItemHistoryView.as_view(), name="item_history"),
     
     # This url will create an ItemRequest object. It needs the id of the current Item object
-    path("items/<int:pk>/request", views.ItemRequestCreateView.as_view(), name="item_request_form"),
+    path("items/request", views.ItemRequestCreateView.as_view(), name="item_request_form"),
     
     # This url will create a UsedItem object. It needs the id of the current Item object
     path("items/use", views.UsedItemCreateView.as_view(), name="item_use_form"),
@@ -29,6 +29,7 @@ urlpatterns = [
     
     # urls for the ItemRequest model
     path("item_requests/", views.ItemRequestView.as_view(), name="item_requests"),
+    path("item_requests/<int:pk>", views.ItemRequestDetailView.as_view(), name="item_request_detail"),
 
     
     # urls for the UseItem model
