f0631c0882c1d9f99495009b99dba5d9bf93adf2
SierraTran <sierra.tran@mail.com>
2025-03-12 15:01:22 -0400

    Add links to notifications; Low stock notifications

--- inventory/signals/handlers.py
+++ inventory/signals/handlers.py
@@ -1,7 +1,9 @@
 from django.db import transaction
-from django.db.models.signals import post_save, pre_delete, post_delete
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.urls import reverse
+from django.utils.html import escape
 from inventory.models import Item, ItemHistory, ItemRequest, UsedItem
 from authentication.models import Notification
 
@@ -55,11 +57,25 @@ def handle_related_records(sender, instance, **kwargs):
 # TODO: Signals that create notifications
 @receiver(post_save, sender=ItemRequest)
 def send_item_request_notification(sender, instance, created, **kwargs):
+    """
+    Creates a notification for Technicians if their item request has been accepted or rejected.
+
+    Args:
+        sender (ItemRequest): The model class that sent the signal.
+        instance (ItemRequest): The instance of the model that has been accepted or rejected.
+        created (bool): True if the action done onto the item is "create", False if otherwise.
+        **kwargs: Additional keyword arguments sent by the signal.
+    """
     if created:
+        # No notification is needed for an item request being created (at least not yet).
         return
     else: 
-        subject = f"Item Request {instance.status}"
-        message = f"Your Item Request for {instance.manufacturer}, {instance.model_part_num} has been " + str(instance.status).lower() + "."
+        subject = escape(f"Item Request {instance.status}")
+        
+        item_request_url = reverse("inventory:item_request_detail", kwargs={"pk": instance.pk})
+        linked_item_request = f'<a href="{item_request_url}">Your Item Request for {instance.manufacturer}, {instance.model_part_num}</a>'
+        
+        message = escape(f"{linked_item_request} has been {str(instance.status).lower()}.")
     with transaction.atomic():
         Notification.objects.create(
             is_read=False,
@@ -67,3 +83,32 @@ def send_item_request_notification(sender, instance, created, **kwargs):
             message=message,
             user=instance.requested_by,
         )
+
+@receiver(post_save, sender=Item)
+def send_low_stock_notification(sender, instance, **kwargs):
+    """
+    Creates a notification for all Superusers if an item is low in stock and how many are left.
+    A new notification for this is created everytime the item is used.
+
+    Args:
+        sender (Item): The model class that sent the signal.
+        instance (Item): The instance of the model that has been accepted or rejected.
+        **kwargs: Additional keyword arguments sent by the signal.
+    """
+    if instance.low_stock:
+        subject = "Low Stock Alert"
+        
+        item_url = reverse("inventory:item_detail", kwargs={"pk": instance.pk})
+        linked_item = f'<a href="{item_url}">{instance}</a>'
+        
+        message = f"{linked_item} is low in stock. {instance.quantity} left."
+        
+        superuser_group = Group.objects.get(name="Superuser")
+        
+        for user in superuser_group.user_set.all():
+            Notification.objects.create(
+                is_read=False,
+                subject=subject,
+                message=message,
+                user=user,
+            )
