c54017cb641bf917e88ee53377f5430ac76d67bf
SierraTran <sierra.tran@mail.com>
2025-03-14 13:44:20 -0400

    Docstrings; New handle_no_permission functions; Refactor login view as class-based view

--- authentication/views.py
+++ authentication/views.py
@@ -1,14 +1,39 @@
+"""
+This module defines class-based views for displaying the home page, login page, notifications, users, and user details.
+
+### Mixins
+    - LoginRequiredMixin:
+        Restricts access to authenticated users. Unauthenticated users will be redirected to the login page. After logging in, they
+        will be redirected back to the original destination preserved by the query parameter defined by `redirect_field_name`.
+    - UserPassesTestMixin:
+        Restricts access based on user-specific conditions.
+
+### Base Classes
+    - LoginView:
+        Handles the login logic and renders the login page. 
+    - ListView:
+        Displays a list of objects.
+    - DetailView:
+        Shows the details of an object.
+    - CreateView:
+        Provides a form for creating new objects in the database.
+    - UpdateView:
+        Provides a form for updating existing objects in the database.
+    - DeleteView:
+        Confirms and processes object deletions.
+"""
+
 from typing import Any
 from django import forms
 from django.shortcuts import render, redirect
 
 from django.contrib import messages
 
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User, Group
+from django.contrib.auth.views import LoginView
 
 from django.urls import reverse, reverse_lazy
 
@@ -24,6 +49,11 @@ from .models import User, Notification
 def home(request):
     """
     Renders the "home.html" template when the user accesses the home page.
+    
+    This method checks if the user is authenticated and retrieves the first group the user belongs to. 
+    The group is then added to the context dictionary under the key "user_group". Atfer that, the 
+    context is passed to the "home.html" template for rendering. A response is returned with the rendered
+    template.
 
     Args:
         request (HttpRequest): The HTTP request object.
@@ -36,47 +66,46 @@ def home(request):
     return render(request, "home.html", context)
 
 
-def login_page(request):
+class DatabaseLoginView(LoginView):
     """
-    Renders the "login.html" template and handles login logic.
-
-    Args:
-        request (HttpRequest): The HTTP request object.
-
-    Returns:
-        HttpResponse: The rendered "login.html" template or a redirect to the home page.
-    """
-    # Redirect authenticaated users to the home page
-    if request.user.is_authenticated:
-        return redirect("authentication:home")
+    Class-based view to handle the login logic and render the login page.
     
-    # Check if the HTTP request method is POST (form submission)
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+    Inherits functionality from:
+        - LoginView
+    (See module docstring for more details on the inherited classes)
+    
+    Methods: 
+        `form_invalid()`: Overrides the base class's `form_invalid` method and extends the behavior to display custom error messages.
+    """
+    
+    def form_invalid(self, form):
+        """
+        Overrides the base class's `form_invalid` method and extends the behavior to display custom error messages.
+        
+        This method first extracts the username and password from the POST request. Then, it checks if the username exists
+        in the database. If the username does not exist, an error message is displayed for an "invalid username". 
+        If the username does exist, it checks if the password associated with the username is correct. If the 
+        password is incorrect (None is returned from the `authenticate` method), an error message will be displayed 
+        or an "invalid password". If the password is correct (a user object is returned by the `authenticate` 
+        method), no error message will be displayed. The base class's `form_invalid` method is then called to retain
+        the default behavior. 
 
-        # Check if a user with the provided username exists
+        Args:
+            form (AuthenticationForm): _description_
+        """
+        username = self.request.POST.get("username")
+        password = self.request.POST.get("password")
+        
         if not User.objects.filter(username=username).exists():
-            # Display an error message if the username does not exist
-            messages.error(request, "Invalid Username ")
-            return redirect("/inventory_database/login/")
-
-        # Authenticate the user with the provided username and password
-        user = authenticate(request, username=username, password=password)
-
-        if user is None:
-            # Display an error message if authentication fails (invalid password)
-            messages.error(request, "Invalid Password ")
-            return redirect("/inventory_database/login/")
+            messages.error(self.request, "Invalid username.")
         else:
-            # Log in the user and redirect to the home page upon successful login
-            login(request, user)
-            return redirect("/inventory_database/")
+            user = authenticate(self.request, username=username, password=password)
+            if user is None:
+                messages.error(self.request, "Invalid password.")
 
-    # Render the login page template (GET request)
-    return render(request, "login.html")
-
-
+        return super().form_invalid(form)
+    
+    
 class NotificationsView(LoginRequiredMixin, ListView):
     """
     Class-based view to list all notifications for the currently logged-in user.
@@ -100,6 +129,7 @@ class NotificationsView(LoginRequiredMixin, ListView):
     context_object_name = "notifications_list"
     
     def get_queryset(self):
+        # TODO: Update docstring
         """
         Retrieves all notifications for the currently logged-in user.
 
@@ -129,6 +159,9 @@ class UsersView(UserPassesTestMixin, ListView):
     def test_func(self) -> bool:
         """
         Verifies if the user is in the "Superuser" group.
+        
+        This method checks the first group the current user belongs to. If the group exists and its name is 
+        'Superuser', it returns True; otherwise, it returns False.
 
         Returns:
             bool: True if the user is in the "Superuser" group, False otherwise.
@@ -164,6 +197,9 @@ class UserDetailsView(UserPassesTestMixin, DetailView):
     def test_func(self) -> bool:
         """
         Verifies if the user is in the "Superuser" group.
+        
+        This method checks the first group the current user belongs to. If the group exists and its name is 
+        'Superuser', it returns True; otherwise, it returns False.
 
         Returns:
             bool: True if the user is in the "Superuser" group, False otherwise.
@@ -221,6 +257,9 @@ class CreateUserView(UserPassesTestMixin, CreateView):
     def test_func(self) -> bool:
         """
         Verifies if the user is in the "Superuser" group.
+        
+        This method checks the first group the current user belongs to. If the group exists and its name is 
+        'Superuser', it returns True; otherwise, it returns False.
 
         Returns:
             bool: True if the user is in the "Superuser" group, False otherwise.
@@ -285,9 +324,11 @@ class UpdateUserView(UserPassesTestMixin, UpdateView):
     template_name = "user_update_form.html"
 
     def test_func(self) -> bool:
-        # TODO: Update docstring
         """
         Verifies if the user is in the "Superuser" group.
+        
+        This method checks the first group the current user belongs to. If the group exists and its name is 
+        'Superuser', it returns True; otherwise, it returns False.
 
         Returns:
             bool: True if the user is in the "Superuser" group, False otherwise.
@@ -320,33 +361,47 @@ class DeleteUserView(UserPassesTestMixin, DeleteView):
     # TODO: Update docstring
     """
     Handles the deletion of a user.
+    
+    Inherits functionality from:
+        - UserPassesTestMixin
+        - CreateView
+    (See module docstring for more details on the inherited classes)
 
     Attributes:
         model (User): The model that the view will operate on.
-        success_url (str): The URL to redirect to after a successful deletion.
+        success_url (str): The URL to redirect to after a successful deletion (resolved using `reverse_lazy`).
+        fail_url (str): The URL to redirect to if the deletion is canceled (resolved using the `get_fail_url` method).
+        
+    Methods:
+        `get_fail_url()`: Returns the URL to redirect to if the deletion is canceled.
+        `test_func()`: Verifies if the user is in the "Superuser" group.
+        `handle_no_permission()`: Renders the 403 page with a message explaining the error.
+        `post()`: Handles the POST request for deleting a user.
     """
 
     model = User
     success_url = reverse_lazy("authentication:users")
 
     def get_fail_url(self):
-        # TODO: Update docstring
         """
         Returns the URL to redirect to if the deletion is canceled.
+        
+        This method uses reverse_lazy to resolve the failure URL with the primary key (pk) of the object
+        being processed and returns it.
 
         Returns:
             str: The URL to redirect to.
         """
-        return reverse_lazy(
-            "authentication:user_details", kwargs={"pk": self.get_object().pk}
-        )
+        return reverse_lazy("authentication:user_details", kwargs={"pk": self.get_object().pk})
 
     fail_url = property(get_fail_url)
 
     def test_func(self) -> bool:
-        # TODO: Update docstring
         """
         Verifies if the user is in the "Superuser" group.
+        
+        This method checks the first group the current user belongs to. If the group exists and its name is 
+        'Superuser', it returns True; otherwise, it returns False.
 
         Returns:
             bool: True if the user is in the "Superuser" group, False otherwise.
@@ -354,7 +409,15 @@ class DeleteUserView(UserPassesTestMixin, DeleteView):
         user_group = self.request.user.groups.first()
         return user_group is not None and user_group.name == "Superuser"
     
-    # TODO: handle no permission function
+    def handle_no_permission(self):
+        """
+        Renders the 403 page with a message explaining the error.
+
+        Returns:
+            HttpResponse: The HTTP response object with the rendered 403 page.
+        """
+        message = "You need to be a Superuser to access this view."
+        return render(self.request, "403.html", {"message": message})
 
     def post(self, request, *args, **kwargs):
         # TODO: Update docstring
