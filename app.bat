0be932ede72d87c4b7d4c3b60032cc258d74009e
SierraTran <sierra.tran@mail.com>
2025-04-02 14:42:00 -0400

    Redid batch file

--- app.bat
+++ app.bat
@@ -1,8 +1,16 @@
-start cmd /k "pip install -r requirements.txt && python manage.py makemigrations && python manage.py migrate && python manage.py runserver"
-@ECHO OFF
-ECHO Hello! The server for the Inventory Database application has been started up for you in another command window.
-ECHO You can close this window when you're ready.
-ECHO To stop the server for the app, simply close the other window.
-ECHO It'll say "C:\Windows\system32\cmd.exe - python manage.py runserver" at the top.
-ECHO You can always spin it back up by clicking the .bat file again.
-PAUSE
+@echo off
+:: Install required packages
+pip install -r requirements.txt
+
+:: Migrations for the web app
+py manage.py makemigrations
+py manage.py migrate
+
+:: Run the server
+start /b py manage.py runserver
+
+:: Wait a moment to make sure the server is up and running
+timeout /t 5 
+
+:: Open a browser to the home page URL
+start "" "http://127.0.0.1:8000/inventory_database"
