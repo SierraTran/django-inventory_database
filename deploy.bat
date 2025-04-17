50b9576334a89fdf63d90485b549b61f709c8425
SierraTran <sierra.tran@mail.com>
2025-04-17 14:30:33 -0400

    Rename "app.bat" to "deploy.bat, Edit to access production server

--- deploy.bat
+++ deploy.bat
@@ -2,15 +2,18 @@
 :: Install required packages
 pip install -r requirements.txt
 
-:: Migrations for the web app
+:: Collect static files
+py manage.py collectstatic --noinput
+
+:: Apply migrations for the web app
 py manage.py makemigrations
 py manage.py migrate
 
 :: Run the server
-start /b py manage.py runserver
+start /b waitress-serve --host=127.0.0.1 --port=8000 inventory_database.wsgi:application
 
 :: Wait a moment to make sure the server is up and running
-timeout /t 5 
+timeout /t 5 /nobreak
 
 :: Open a browser to the home page URL
 start "" "http://127.0.0.1:8000/inventory_database"
