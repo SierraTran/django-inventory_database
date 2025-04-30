8820019be40aa77118e5cc3f83e1d88e3d1677d2
SierraTran <sierra.tran@mail.com>
2025-04-30 07:20:55 -0400

    Remove trailing spaces and shorten long lines

--- inventory/signals/handlers.py
+++ inventory/signals/handlers.py
@@ -1,7 +1,7 @@
 """
 This module contains signal handlers for the Inventory app.
 
-### Imported Signals
+Imported Signals
     - post_save: Sent after a model's `save` method is called.
     - pre_delete: Sent just before a model's `delete` method is called.
 """
@@ -14,7 +14,8 @@ from django.utils.html import escape
 from inventory.models import Item, ItemHistory, ItemRequest, UsedItem
 from authentication.models import Notification
 
-# NOTE: Regardless of being used or not, `sender` and `**kwargs` parameters need to be included in the other signal handlers to avoid errors.
+# NOTE: Regardless of being used or not, `sender` and `**kwargs` parameters need to be included in
+# the other signal handlers to avoid errors.
 
 
 @receiver(post_save, sender=Item)
@@ -22,9 +23,11 @@ def create_or_update_item_history(sender, instance, created, **kwargs):
     """
     Creates an ItemHistory record when an item has been created or updated.
     
-    This method first checks if the item has been created or updated. If the item has been created, the action is set to "create".
-    If the item has been updated, the action is set to "update". The changes are then recorded in a newly created ItemHistory record.
-    Records with the "update" action will have the fields that have been changed and their old values recorded in the `changes` field.
+    This method first checks if the item has been created or updated. If the item has been created,
+    the action is set to "create". If the item has been updated, the action is set to "update". The
+    changes are then recorded in a newly created ItemHistory record. Records with the "update"
+    action will have the fields that have been changed and their old values recorded in the 
+    `changes` field.
 
     Arguments:
         sender (Item): The model class that sent the signal.
@@ -32,7 +35,7 @@ def create_or_update_item_history(sender, instance, created, **kwargs):
         created (bool): True if the action done onto the item is "create", False if otherwise.
         **kwargs: Additional keyword arguments sent by the signal.
     """
-    
+
     action = "create" if created else "update"
     changes = "Created and added to the database."
     if not created:
@@ -66,16 +69,17 @@ def handle_related_records(sender, instance, **kwargs):
     # QUESTION: Delete this line and make UsedItem not directly reference the item so it can stay in the database?
     UsedItem.objects.filter(item=instance).delete()
     
-# NOTE: The following signal handlers create notifications for users when certain events happen in they system.
+# NOTE: The following signal handlers create notifications for users when certain events happen in 
+# they system.
 @receiver(post_save, sender=ItemRequest)
 def send_item_request_notification(sender, instance, created, **kwargs):
     """ 
-    Creates a notification for Technicians if their item request has been accepted or rejected by a Superuser.
-    Creates a notification for Superusers if a new item request has been created.
+    Creates a notification for Technicians if their item request has been accepted or rejected by a
+    Superuser, or creates a notification for Superusers if a new item request has been created.
     
-    This method checks if the item request is created or not. If it is, a notification is created for all Superusers.
-    If the item request is not created, it checks if the status of the item request has changed. If it has, a 
-    notification is created for the user that requested the item.
+    This method checks if the item request is created or not. If it is, a notification is created
+    for all Superusers. If the item request is not created, it checks if the status of the item
+    request has changed. If it has, a notification is created for the user that requested the item.
 
     Args:
         sender (ItemRequest): The model class that sent the signal.
@@ -87,7 +91,7 @@ def send_item_request_notification(sender, instance, created, **kwargs):
     message = None
     user = None
     instance_url = instance.get_absolute_url()
-    
+
     if created:
         subject = "New Item Request"
         message = f"There's a new item request for {instance.manufacturer}, {instance.model_part_num}. " \
@@ -101,12 +105,12 @@ def send_item_request_notification(sender, instance, created, **kwargs):
                 user=user,
             )
         return
-    
+
     if 'status' in instance.tracker.changed(): 
         # Display the new status of the item request in the subject of the notification.
         subject = escape(f"Item Request {instance.status}")
         # Create a link to the item request to include in the message.
-        linked_item_request = f'<a href="{instance_url}">Your Item Request for {instance.manufacturer}, {instance.model_part_num}</a>'        
+        linked_item_request = f'<a href="{instance_url}">Your Item Request for {instance.manufacturer}, {instance.model_part_num}</a>'
         # Explain the new status of the item request and include its link in the message.
         message = f"{linked_item_request} has been {str(instance.status).lower()} by {instance.status_changed_by.username}. " \
                     "If you're all set with your item request, please delete it."
@@ -132,14 +136,11 @@ def send_low_stock_notification(sender, instance, **kwargs):
     """
     if instance.low_stock:
         subject = "Low Stock Alert"
-        
         item_url = instance.get_absolute_url()
-
-        linked_item = f'<a href="{item_url}">{instance}</a>'        
+        linked_item = f'<a href="{item_url}">{instance}</a>'
         message = f"{linked_item} is low in stock. {instance.quantity} left."
-        
         superuser_group = Group.objects.get(name="Superuser")
-        
+
         for user in superuser_group.user_set.all():
             Notification.objects.create(
                 is_read=False,
