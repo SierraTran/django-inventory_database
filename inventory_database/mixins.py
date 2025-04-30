29540917e1f52bbadefe8b6cf6a9d494912a5150
SierraTran <sierra.tran@mail.com>
2025-04-30 07:10:17 -0400

    Remove trailing spaces and shorten long lines

--- inventory_database/mixins.py
+++ inventory_database/mixins.py
@@ -1,10 +1,11 @@
 """
-    This module holds custon mixins for the project, specifically for allowing users in certain groups to access certain views.
-    The mixins are used in the views.py file of both apps.
+    This module holds custon mixins for the project, specifically for allowing users in certain
+    groups to access certain views. The mixins are used in the views.py file of both apps.
     
     ### Mixins:
         - UserPassesTestMixin: 
-            This mixin is used for checking if a user passes a certain test. It is used as a base class for the other mixins.
+            This mixin is used for checking if a user passes a certain test. It is used as a base
+            class for the other mixins.
         
             See more info in the Django documentation:
             https://docs.djangoproject.com/en/3.2/topics/auth/default/#django.contrib.auth.mixins.UserPassesTest)
@@ -20,21 +21,22 @@ class SuperuserRequiredMixin(UserPassesTestMixin):
 
     Methods:
         `test_func()`: Verifies if the user is in the "Superuser" group.
-        `handle_no_permission()`: Renders the 403 page with a message explaining the reason for the error.
+        `handle_no_permission()`: Renders the 403 page with a message explaining the reason for the
+            error.
     """
     def test_func(self) -> bool:
         """
         Verifies if the user is in the "Superuser" group.
         
-        This method checks the first group the current user belongs to. If the group exists and its name is 
-        "Superuser", it returns True; otherwise, it returns False.
+        This method checks the first group the current user belongs to. If the group exists and its
+        name is "Superuser", it returns True; otherwise, it returns False.
 
         Returns:
             bool: True if the user is in the "Superuser" group, False otherwise.
         """
         user_group = self.request.user.groups.first()
         return user_group is not None and user_group.name == "Superuser"
-    
+
     def handle_no_permission(self):
         """
         Renders the 403 page with a message explaining the reason for the error.
@@ -44,7 +46,7 @@ class SuperuserRequiredMixin(UserPassesTestMixin):
         """
         message = "You need to be a Superuser to access this view."
         return HttpResponseForbidden(render(self.request, "403.html", {"message": message}))
-    
+
 
 class SuperuserOrTechnicianRequiredMixin(UserPassesTestMixin):
     """
@@ -52,21 +54,22 @@ class SuperuserOrTechnicianRequiredMixin(UserPassesTestMixin):
 
     Methods:
         `test_func()`: Verifies if the user is in the "Superuser" or "Technician" group.
-        `handle_no_permission()`: Renders the 403 page with a message explaining the reason for the error.
+        `handle_no_permission()`: Renders the 403 page with a message explaining the reason for the
+            error.
     """
     def test_func(self) -> bool:
         """
         Verifies if the user is in the "Superuser" or "Technician" group.
         
-        This method checks the first group the current user belongs to. If the group exists and its name is 
-        "Superuser" or "Technician", it returns True; otherwise, it returns False.
+        This method checks the first group the current user belongs to. If the group exists and its
+        name is "Superuser" or "Technician", it returns True; otherwise, it returns False.
 
         Returns:
             bool: True if the user is in the "Superuser" or "Technician" group, False otherwise.
         """
         user_group = self.request.user.groups.first()
         return user_group is not None and user_group.name in ["Superuser", "Technician"]
-    
+
     def handle_no_permission(self):
         """
         Renders the 403 page with a message explaining the reason for the error.
@@ -76,29 +79,30 @@ class SuperuserOrTechnicianRequiredMixin(UserPassesTestMixin):
         """
         message = "You need to be a Superuser or Technician to access this view."
         return HttpResponseForbidden(render(self.request, "403.html", {"message": message}))
-    
-    
+
+
 class TechnicianRequiredMixin(UserPassesTestMixin):
     """
     A mixin that allows only users in the "Technician" group to access the view.
 
     Methods:
         `test_func()`: Verifies if the user is in the "Technician" group.
-        `handle_no_permission()`: Renders the 403 page with a message explaining the reason for the error.
+        `handle_no_permission()`: Renders the 403 page with a message explaining the reason for the
+            error.
     """
     def test_func(self) -> bool:
         """
         Verifies if the user is in the "Technician" group.
         
-        This method checks the first group the current user belongs to. If the group exists and its name is 
-        "Technician", it returns True; otherwise, it returns False.
+        This method checks the first group the current user belongs to. If the group exists and its
+        name is "Technician", it returns True; otherwise, it returns False.
 
         Returns:
             bool: True if the user is in the "Technician" group, False otherwise.
         """
         user_group = self.request.user.groups.first()
         return user_group is not None and user_group.name == "Technician"
-    
+
     def handle_no_permission(self):
         """
         Renders the 403 page with a message explaining the reason for the error.
@@ -108,29 +112,30 @@ class TechnicianRequiredMixin(UserPassesTestMixin):
         """
         message = "You need to be a Technician to access this view."
         return HttpResponseForbidden(render(self.request, "403.html", {"message": message}))
-    
-    
+
+
 class InternRequiredMixin(UserPassesTestMixin):
     """
     A mixin that allows only users in the "Intern" group to access the view.
 
     Methods:
         `test_func()`: Verifies if the user is in the "Intern" group.
-        `handle_no_permission()`: Renders the 403 page with a message explaining the reason for the error.
+        `handle_no_permission()`: Renders the 403 page with a message explaining the reason for the
+            error.
     """
     def test_func(self) -> bool:
         """
         Verifies if the user is in the "Intern" group.
         
-        This method checks the first group the current user belongs to. If the group exists and its name is 
-        "Intern", it returns True; otherwise, it returns False.
+        This method checks the first group the current user belongs to. If the group exists and its
+        name is "Intern", it returns True; otherwise, it returns False.
 
         Returns:
             bool: True if the user is in the "Intern" group, False otherwise.
         """
         user_group = self.request.user.groups.first()
         return user_group is not None and user_group.name == "Intern"
-    
+
     def handle_no_permission(self):
         """
         Renders the 403 page with a message explaining the reason for the error.
@@ -140,4 +145,3 @@ class InternRequiredMixin(UserPassesTestMixin):
         """
         message = "You need to be a Intern to access this view."
         return HttpResponseForbidden(render(self.request, "403.html", {"message": message}))
-    
