e7351f0f1c082d3c0efef3cf255fba99c2c4663f
SierraTran <sierra.tran@mail.com>
2025-05-02 12:40:22 -0400

    Update admin.py
    - Group fields into fieldsets for improved readability

--- authentication/admin.py
+++ authentication/admin.py
@@ -4,8 +4,13 @@ from .models import Notification
 
 
 class NotificationAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["is_read"]}),
+        ("Content", {"fields": ["subject", "message"]}),
+        ("Recipient", {"fields": ["user"]})
+    ]
     ordering = ["-timestamp"]
-    
-    
+
+
 # Register your models here.
 admin.site.register(Notification, NotificationAdmin)
