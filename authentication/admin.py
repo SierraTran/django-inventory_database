0f9a82377753192fd765f2319de2fb48e4d102cf
SierraTran <sierra.tran@mail.com>
2025-05-02 13:49:16 -0400

    Update admin.py (authentication)
    - Write short module docstring
    - Write class docstring

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,9 +1,24 @@
-from django.contrib import admin
+"""
+This module defines the admin interface for the authentication app.
+It customizes the admin views for the Notification model.
+"""
 
+from django.contrib import admin
 from .models import Notification
 
 
 class NotificationAdmin(admin.ModelAdmin):
+    """
+    Custom admin interface for the Notification model.
+    
+    Attributes:
+        fieldsets (list): Fieldsets for the admin form layout.
+            - None: Default fieldset with is_read field.
+            - Content: Fieldset for the notification content with subject and message fields.
+            - Recipient: Fieldset for the recipient with user field.
+        ordering (list): Default ordering of notifications in the list view
+            - timestamp: Order reverse-chronologically by timestamp
+    """
     fieldsets = [
         (None, {"fields": ["is_read"]}),
         ("Content", {"fields": ["subject", "message"]}),
